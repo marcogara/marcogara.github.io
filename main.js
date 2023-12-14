13710d9f4ccad0c150ab9cc3bc2c680e4c601d6d
Marco <marcogara24@gmail.com>
2023-12-14 16:37:07 +0100

    add car project canvas

---
+++ main.js
@@ -0,0 +1,7 @@
+const canvas=document.getElementById("myCanvas");
+canvas.height=window.innerHeight;
+canvas.width=400;
+
+const ctx = canvas.getContext("2d");
+const car= new Car(100,100,30,50);
+car.draw(ctx);
