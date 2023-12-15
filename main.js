ee900aa0f60ea275b16c1b0d3cde8fe147c1e145
Marco <marcogara24@gmail.com>
2023-12-15 16:12:05 +0100

    update car project

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const canvas=document.getElementById("myCanvas");
 canvas.width=400;
 
 const ctx = canvas.getContext("2d");
-const car= new Car(100,100,30,50);
+const road=new Road(canvas.width/2,canvas.width*0.9);
+const car=new Car(100,100,30,50);
 car.draw(ctx);
 
 animate();
@@ -11,7 +12,9 @@ animate();
 
 function animate(){
     car.update();
+
     canvas.height=window.innerHeight;
+    road.draw(ctx);
     car.draw(ctx);
     requestAnimationFrame(animate);
 }
